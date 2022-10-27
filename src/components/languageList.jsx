c49e0be72496a02d1d95edeb66e48f60917d8361
Hanh Duong <dg.hanh117@gmail.com>
2022-10-27 10:01:28 +0300

    add submitForm

--- src/components/languageList.jsx
+++ src/components/languageList.jsx
@@ -1,42 +1,42 @@
 export const languageList = [{
     pronoun : ["Minä", "Sinä", "Hän", "Me", "Te", "He"],
-    verb : ["Menen", "Menet", "Menee", "Menemme", "Menette", "Menevät"],
-    translate : ["I go", "You go", "She/He goes", "We go", "You go", "They go" ],
+    verb : ["odotan bussia", "odotat bussia", "odottaa bussia", "odotamme bussia", "odotatte bussia", "odottavat bussia"],
+    translate : ["I am waiting for a bus", "You are waiting for a bus", "She/He is waiting for a bus", "We are waiting for a bus", "You are waiting for a bus", "They are waiting for a bus" ],
     color : ["lightgreen", "lightpink", "lightblue", "lightgreen", "lightblue", "lightpink"],
-    url : ["/image/go.JPG", "https://www.flaticon.com/free-icon/cleaning_8685798?related_id=8685798&origin=pack"]
+    url : ["/image/odottaa.svg", "/image/odottaa.svg", "/image/odottaa.svg", "/image/odottaa2.svg", "/image/odottaa2.svg", "/image/odottaa2.svg" ]
 },
 {
-    pronoun : ["M", "S", "H", "M", "T", "H"],
-    verb : ["M", "Menet", "Menee", "Menemme", "Menette", "Menevät"],
-    translate : ["I go", "You go", "She/He goes", "We go", "You go", "They go" ],
+    pronoun : ["Minä", "Sinä", "Hän", "Me", "Te", "He"],
+    verb : ["syön keittoa", "syöt keittoa", "syö keittoa", "syömme keittoa", "syötte keittoa", "syövät keittoa"],
+    translate : ["I eat soup", "You eat soup", "She/He eats soup", "We eat soup", "You eat soup", "They eat soup" ],
     color : ["green", "lightpink", "lightblue", "lightgreen", "lightblue", "lightpink"],
     url : ["https://www.flaticon.com/free-icon/cleaning_8685798?related_id=8685798&origin=pack", "https://www.flaticon.com/free-icon/cleaning_8685798?related_id=8685798&origin=pack"]
 },
 {
-    pronoun : ["a", "c", "Hf", "g", "fe", "g"],
-    verb : ["Menen", "Menet", "Menee", "Menemme", "Menette", "Menevät"],
-    translate : ["I go", "You go", "She/He goes", "We go", "You go", "They go" ],
+    pronoun : ["Minä", "Sinä", "Hän", "Me", "Te", "He"],
+    verb : ["kävelen metsässä", "kävelen metsässä", "Kävelen metsässä", "Kävelen metsässä", "Kävelen metsässä", "Kävelen metsässä"],
+    translate : ["I walk in the woods", "You walk in the woods", "She/He walks in the woods", "We walk in the woods", "You walk in the woods", "They walk in the woods" ],
     color : ["lightgreen", "lightpink", "lightblue", "lightgreen", "lightblue", "lightpink"],
     url : ["https://www.flaticon.com/free-icon/cleaning_8685798?related_id=8685798&origin=pack", "https://www.flaticon.com/free-icon/cleaning_8685798?related_id=8685798&origin=pack"]
 },
 {
     pronoun : ["Minä", "Sinä", "Hän", "Me", "Te", "He"],
-    verb : ["Menen", "Menet", "Menee", "Menemme", "Menette", "Menevät"],
-    translate : ["I go", "You go", "She/He goes", "We go", "You go", "They go" ],
+    verb : ["Menen kauppaan", "Menet kauppaan", "Menee kauppaan", "Menemme kauppaan", "Menette kauppaan", "Menevät kauppaan"],
+    translate : ["I go to the store", "You go", "She/He goes", "We go", "You go", "They go" ],
     color : ["lightgreen", "lightpink", "lightblue", "lightgreen", "lightblue", "lightpink"],
     url : ["https://www.flaticon.com/free-icon/cleaning_8685798?related_id=8685798&origin=pack", "https://www.flaticon.com/free-icon/cleaning_8685798?related_id=8685798&origin=pack"]
 },
 {
     pronoun : ["Minä", "Sinä", "Hän", "Me", "Te", "He"],
-    verb : ["Menen", "Menet", "Menee", "Menemme", "Menette", "Menevät"],
-    translate : ["I go", "You go", "She/He goes", "We go", "You go", "They go" ],
+    verb : ["Siivoan keittiön", "Menet", "Menee", "Menemme", "Menette", "Menevät"],
+    translate : ["I clean the kitchen", "You go", "She/He goes", "We go", "You go", "They go" ],
     color : ["lightgreen", "lightpink", "lightblue", "lightgreen", "lightblue", "lightpink"],
     url : ["https://www.flaticon.com/free-icon/cleaning_8685798?related_id=8685798&origin=pack", "https://www.flaticon.com/free-icon/cleaning_8685798?related_id=8685798&origin=pack"]
 },
 {
     pronoun : ["Minä", "Sinä", "Hän", "Me", "Te", "He"],
-    verb : ["Menen", "Menet", "Menee", "Menemme", "Menette", "Menevät"],
-    translate : ["I go", "You go", "She/He goes", "We go", "You go", "They go" ],
+    verb : ["tarvitsen apua", "tarvitset apua", "tarvitsee apua", "tarvitsemme apua", "tarvitsette apua", "tarvitsevat apua"],
+    translate : ["I need help", "You go to the store", "She/He goes to the store", "We go", "You go", "They go" ],
     color : ["lightgreen", "lightpink", "lightblue", "lightgreen", "lightblue", "lightpink"],
     url : ["https://www.flaticon.com/free-icon/cleaning_8685798?related_id=8685798&origin=pack", "https://www.flaticon.com/free-icon/cleaning_8685798?related_id=8685798&origin=pack"]
 }
